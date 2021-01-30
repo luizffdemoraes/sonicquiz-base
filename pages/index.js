082bc064969eb83d0e6e6f555767159623def830
luizffdemoraes <lffm1994@gmail.com>
2021-01-30 01:33:59 -0300

    Final aula 5

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
-import {useRouter} from 'next/router';
+import { motion } from 'framer-motion';
+import { useRouter } from 'next/router';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import Link from '../src/components/Link';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
@@ -12,16 +14,12 @@ import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
 // const BackgroundImage = styled.div`
-//    background-image: url(${db.bg});
-//    flex: 1;
-//    background-size: cover;
-//    background-position: center;
-//  `;
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -37,31 +35,39 @@ const QuizContainer = styled.div`
 export default function Home() {
   const router = useRouter();
   const [name, setName] = React.useState('');
-  console.log('useState', name, setName);
 
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>
+          SonicQuiz -
+          {db.title}
+        </title>
+      </Head>
       <QuizContainer>
-        <Head>
-          <title>SonicQuiz - Modelo Base</title>
-
-        </Head>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
-            <h1>Sonic</h1>
-
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function(infosDoEvento){
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
-              
-              
               router.push(`/quiz?name=${name}`);
               console.log('Fazendo uma submissão por meio do react');
-              // router manda para a próxima pagina
-            }}>
-             <Input
+            }}
+            >
+              <Input
                 name="nomeDoUsuario"
                 onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
                 placeholder="Diz ai seu nome"
@@ -71,23 +77,56 @@ export default function Home() {
                 {`Jogar ${name}`}
               </Button>
             </form>
-           
           </Widget.Content>
-
         </Widget>
-        <Widget>
-          <Widget.Header>
+
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
+          <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-          </Widget.Header>
-          <Widget.Content>
-            <p>lorem ipsum...</p>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
-        <Footer />
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/luizffdemoraes" />
     </QuizBackground>
-
   );
 }
